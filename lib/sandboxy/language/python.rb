af1f850b1c7387729053ff0c78536442b83aff8a
Daniel Bergmann <brgmnn@users.noreply.github.com>
2016-08-27 15:01:43 -0700

    Added python ERB functions.

--- lib/sandboxy/language/python.rb
+++ lib/sandboxy/language/python.rb
@@ -2,4 +2,18 @@ module Sandboxy::Language::Python
   extend Sandboxy::Language::Base
 
   self::CMD = 'python'.freeze
+
+  def pass
+    "print '#{@id} test passed'"
+  end
+
+  def fail
+    "print '#{@id} test failed'"
+  end
+
+  def assert
+    @output << 'if ('
+    yield
+    @output << "):\n  #{pass}\nelse:\n  #{fail}\n"
+  end
 end
